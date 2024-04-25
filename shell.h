7b91ddde7b70ab0aaac87cfe48493c38042ac5b7
AnthonyCointre <8732@holbertonstudents.com>
2024-04-25 09:37:37 +0200

    creation shell.h

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SIMPLE_SHELL_H_
+#define SIMPLE_SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void display_prompt(void);
+void interactive_mode(void);
+void non_interactive_mode(char *command);
+int run_command(char *input);
+
+#endif /*SIMPLE_SHELL_H_*/
